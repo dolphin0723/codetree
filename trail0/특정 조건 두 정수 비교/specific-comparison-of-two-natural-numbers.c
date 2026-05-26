62e0ab24fe3edb59c3cab2d0602f1481c85d18c4
ya999gom <52690408+dolphin0723@users.noreply.github.com>
2026-05-26 15:21:57 +0900

    [Passed] 특정 조건 두 정수 비교 (1ms, 0MB)

---
+++ trail0/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+
+    int a,b;
+
+    scanf("%d %d", &a, &b);
+
+    if(a<b)
+        printf("%d ", 1);
+    else
+        printf("%d ", 0);
+
+    if(a==b)
+        printf("%d", 1);
+    else
+        printf("%d", 0);
+
+    return 0;
+}
